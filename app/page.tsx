8336a06b2e07733cfd15f08852bb3717354aeb5c
goshiEta <tamerika563@gmail.com>
2024-09-17 15:07:32 +0900

    ファビコンを更新

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import styles from "@/styles/page.module.css";
 
 export default function Home() {
   return (
-    <div id={styles.top}></div>
+    <div id={styles.top}>
+      
+    </div>
   );
 }
