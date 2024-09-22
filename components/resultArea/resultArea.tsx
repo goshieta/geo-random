15975aa4ba01c3b42dacad52fbebb97a7e3e4465
goshiEta <tamerika563@gmail.com>
2024-09-22 13:34:41 +0900

    wikipediaを追加

--- components/resultArea/resultArea.tsx
+++ components/resultArea/resultArea.tsx
@@ -8,24 +8,31 @@ const RandomMap = dynamic(() => import("@/components/resultArea/map"), {
 
 export default async function ResultArea() {
   const data = await getRandomPosition();
-  let name = "名称のない公園";
+  let name = "名称不明の公園";
   if (data.tags && data.tags.name) {
     name = data.tags.name;
   }
   return (
     <div id={styles.result}>
       <h1>{name}</h1>
-      <a
-        href={`https://www.google.com/maps/place/${data.lat},${data.lon}`}
-        target="_blank"
-      >
-        google mapでアクセス
-      </a>
-      <a
-        href={`https://map.yahoo.co.jp/place?&lat=${data.lat}1&lon=${data.lon}`}
-      >
-        Yahoo地図でアクセス
-      </a>
+      <div id={styles.links}>
+        <a
+          href={`https://www.google.com/maps/place/${data.lat},${data.lon}`}
+          target="_blank"
+        >
+          Google Map
+        </a>
+        <a
+          href={`https://map.yahoo.co.jp/place?&lat=${data.lat}1&lon=${data.lon}`}
+        >
+          Yahoo!地図
+        </a>
+        {data.tags && data.tags.wikipedia && (
+          <a href={`https://ja.wikipedia.org/wiki/${data.tags.wikipedia}`}>
+            Wikipedia
+          </a>
+        )}
+      </div>
       <RandomMap lat={data.lat} lon={data.lon} name={name} />
     </div>
   );
