fe1734b5581c82d2c1c78084018f6d9a89e6b9a1
goshiEta <tamerika563@gmail.com>
2024-09-22 15:06:46 +0900

    結果ページのデザインを整えた

--- components/resultArea/resultArea.tsx
+++ components/resultArea/resultArea.tsx
@@ -1,6 +1,7 @@
 import getRandomPosition from "@/lib/getRandomPosition";
 import styles from "@/styles/result/result.module.css";
 import dynamic from "next/dynamic";
+import { ReactNode } from "react";
 
 const RandomMap = dynamic(() => import("@/components/resultArea/map"), {
   ssr: false,
@@ -15,25 +16,48 @@ export default async function ResultArea() {
   return (
     <div id={styles.result}>
       <h1>{name}</h1>
+      <div id={styles.map_area}>
+        <RandomMap lat={data.lat} lon={data.lon} name={name} />
+      </div>
       <div id={styles.links}>
-        <a
+        <InsertLink
           href={`https://www.google.com/maps/place/${data.lat},${data.lon}`}
-          target="_blank"
+          imgSrc="/serviceIcon/google_map.png"
         >
           Google Map
-        </a>
-        <a
+        </InsertLink>
+        <InsertLink
           href={`https://map.yahoo.co.jp/place?&lat=${data.lat}1&lon=${data.lon}`}
+          imgSrc="/serviceIcon/yahoo_map.png"
         >
           Yahoo!地図
-        </a>
+        </InsertLink>
         {data.tags && data.tags.wikipedia && (
-          <a href={`https://ja.wikipedia.org/wiki/${data.tags.wikipedia}`}>
+          <InsertLink
+            href={`https://ja.wikipedia.org/wiki/${data.tags.wikipedia}`}
+            imgSrc="/serviceIcon/wikipedia.png"
+          >
             Wikipedia
-          </a>
+          </InsertLink>
         )}
       </div>
-      <RandomMap lat={data.lat} lon={data.lon} name={name} />
     </div>
+  );
+}
+
+function InsertLink({
+  href,
+  imgSrc,
+  children,
+}: {
+  href: string;
+  imgSrc: string;
+  children: ReactNode;
+}) {
+  return (
+    <button className={styles.open_link}>
+      <img src={imgSrc} alt={children?.toString()} width={32} height={32} />
+      <p>{children}</p>
+    </button>
   );
 }
